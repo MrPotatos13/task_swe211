8c8ca9739fb5aea4f3be8c8d4ae73829cfcb4868
MrPotatos13 <129624694+MrPotatos13@users.noreply.github.com>
2024-04-21 21:17:58 +0300

    Update TASK.py
    add if statement if it wasn't exists and print the error in red

--- TASK.py
+++ TASK.py
@@ -1,27 +1,31 @@
-file = open("test.txt", "r")
+import os
 print()
-# next line will copy the txt from file to the var txt
-txt = ""
-for t in file:
-    txt = txt + t
+x=input("Please enter the file name or path\n")
+if ( os.path.isfile(x)):
 
-print("The file content before sorting:")
-print(txt)
-print()
+    file = open(x, "r")
+    # next line will copy the txt from file to the var txt
+    txt = file.read()
 
-list = []
-list = txt.split(" ")
-# it will sepreate each word
-number = []
-word = []
-# check if the word is number or alphabit
-for x in list:
-    if x.isdigit():
-        number.append(x)
-    elif x.isalpha():
-        word.append(x)
-print("After sorting:")
-print()
-print("Number list: "+str(number))
-print()
-print("Word list: "+str(word))
+    print("The file content before sorting:")
+    print(txt)
+    print()
+
+    list = []
+    list = txt.split(" ")
+    # it will sepreate each word
+    number = []
+    word = []
+    # check if the word is number or alphabit
+    for x in list:
+        if x.isdigit():
+         number.append(x)
+        elif x.isalpha():
+         word.append(x)
+    print("After sorting:")
+    print()
+    print("Number list: "+str(number))
+    print()
+    print("Word list: "+str(word))
+else:
+    print("\033[91m{}\033[00m".format("There is no file or path exists has name "+x))
